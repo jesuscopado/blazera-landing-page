43b8aead512322779088bcade6aeff79c4d20635
jesuscopado <jcr.22@hotmail.com>
2020-03-21 13:44:27 +0200

    Named attributes in the form

--- src/subpages/End.js
+++ src/subpages/End.js
@@ -7,7 +7,7 @@ export default function End() {
       className="container-fluid"
       style={{ backgroundColor: "#FFCD6C", color: "black" }}
     >
-      <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+      <form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
       <div className="container">
         <div className="flexbox">
           <Fade left>
@@ -17,7 +17,8 @@ export default function End() {
               <div className="row my-4">
                 <div className="col-lg-6 mb-4">
                   <input
-                    type="name"
+                    type="text"
+                    name="name"
                     className="inputBox"
                     placeholder="Your Name"
                   />
@@ -25,12 +26,13 @@ export default function End() {
                 <div className="col-lg-6 mb-4">
                   <input
                     type="email"
+                    name="email"
                     className="inputBox"
                     placeholder="Your Email"
                   />
                 </div>
               </div>
-              <textarea rows = "2" className="inputBox mb-4" placeholder="Your Message" />
+              <textarea name="message" rows="5" className="inputBox mb-4" placeholder="Your Message" />
               <button className="btn btn-light mb-4 py-3">
                 Submit
               </button>   
